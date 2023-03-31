01f6fa6523444643d673785cc7d2f8cbea2703fd
Heitor Carvalho de Almeida Neto <heitorh3@gmail.com>
2023-03-31 15:57:29 -0300

    Criando a tabela de photo

--- mysql/initial_data/bootstrap.sql
+++ mysql/initial_data/bootstrap.sql
@@ -1,5 +1,6 @@
 CREATE DATABASE treinamentoPhp;
 USE treinamentoPhp;
+
 CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(45) DEFAULT NULL,
@@ -8,3 +9,10 @@ CREATE TABLE `users` (
 ) ENGINE = InnoDB AUTO_INCREMENT = 0 DEFAULT CHARSET = utf8;
 INSERT INTO users (name, email)
 VALUES ('Heitor Neto', 'heitorh3@gmail.com');
+
+CREATE TABLE photo {
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userid` int(11) DEFAULT NOT NULL,
+  `path` varchar(45) DEFAULT NULL,  
+   FOREIGN KEY (`userid`) REFERENCES `users`(`userid`)
+}
